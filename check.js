9d52ee135d6db0a821ff06731f0da33a31951560
최승교 <choisk1116@gmail.com>
2020-08-21 09:02:28 +0900

    [EDIT] 20200821 choisk many things

--- check.js
+++ check.js
@@ -1,21 +1,18 @@
-var score = localStorage.getItem('score')
+var score = localStorage.getItem("score");
 const number1 = document.getElementById("number1");
 const number2 = document.getElementById("number2");
-const number3 = document.getElementById('number3')
-const number4 = document.getElementById('number4')
+const number3 = document.getElementById("number3");
+const number4 = document.getElementById("number4");
 
-// document.body.insertAdjacentHTML("beforeend", `${score[0]}`);
-// document.body.insertAdjacentHTML("beforeend", `${score[1]}`);
-
-if(score==='100'){
-number1.innerText = 1
+if (score === "100") {
+  number1.innerText = 1;
 }
-number2.innerText = score[1]
+number2.innerText = score[1];
 
-number4.innerText = score[2]
-number3.innerText = '%'
+number3.innerText = score[2];
+number4.innerText = "%";
 
-document.getElementById('gotomain').onclick = function(){
-    localStorage.clear()
-    window.location.href = "input.html"
-}
+document.getElementById("gotomain").onclick = function () {
+  localStorage.clear();
+  window.location.href = "input.html";
+};
